6887f53f6750016d64f4bf64750f5a159322f531
Mike Stubna <mike@stubna.com>
2020-03-17 12:05:02 -0400

    updates
    - fit offset separately
    - improve plots

--- plot_trends.py
+++ plot_trends.py
@@ -11,6 +11,7 @@ dirname = pathlib.Path(__file__).resolve().parent
 pd.set_option('display.max_columns', 200)
 pd.set_option('display.max_rows', 200)
 colors = {
+  'very_light_gray': '#ececec',
   'light_gray': '#b6b6b6',
   'medium_gray': '#929292',
   'very_dark_gray': '#414141',
@@ -39,20 +40,44 @@ countries = { key: convert_to_ts(data, key) for key in country_names}
 
 #%% figure 1
 dat = [
-  ('China', 0, 'light_gray'),
-  ('Korea, South', 33, 'medium_gray'),
-  ('Italy', 37, 'very_dark_gray'),
-  ('Iran', 38, 'light_blue'),
-  ('Spain', 45, 'light_purple'),
-  ('Germany', 45, 'light_green'),
-  ('France', 45, 'light_yellow'),
-  ('US', 47, 'orange')
+  { 'name': 'China', 'color': 'light_gray' },
+  { 'name': 'Korea, South', 'color': 'medium_gray' },
+  { 'name': 'Italy', 'color': 'very_dark_gray' },
+  { 'name': 'Iran', 'color': 'light_blue' },
+  { 'name': 'Spain', 'color': 'light_purple' },
+  { 'name': 'Germany', 'color': 'light_green' },
+  { 'name': 'France', 'color': 'light_yellow' },
+  { 'name': 'US', 'color': 'orange' },
 ]
 
+#%% compute offset for each country that best fits onset of epidemic (i.e., first 7 days)
+def comparison_to_china_penalty (df, offset):
+  china_counts = countries['China']['count'].to_numpy()
+  counts = df['count'].to_numpy()
+  residuals = []
+  for i in range(0, 7):
+    if i + offset < len(counts):
+      residuals.append(china_counts[i] - counts[i + offset])
+    else:
+      residuals.append(0)
+  return np.power(residuals, 2).sum()
+
+def find_optimal_offset (df):
+  penalties = []
+  for offset in range(len(df)):
+    penalties.append(comparison_to_china_penalty(df, offset))
+  return np.argmin(penalties)
+
+for d in dat:
+  d['offset'] = find_optimal_offset(countries[d['name']])
+print(np.array(dat))
+
+#%% plot the initial epidemic onsets for each country adjusted by their onset offsets
 fig = plt.figure(figsize=(12, 6))
 ax = fig.add_subplot(111)
 
-for country_name, offset, color_key in dat:
+for d in dat:
+  country_name, offset, color_key = itemgetter('name', 'offset', 'color')(d)
   country = countries[country_name]
   ax.plot(
     country['date'] - np.timedelta64(offset, 'D'),
@@ -61,44 +86,50 @@ for country_name, offset, color_key in dat:
     color=colors[color_key]
   )
 
-plt.xlim((np.datetime64('2020-01-01'),np.datetime64('2020-03-01')))
+plt.xlim((np.datetime64('2020-01-22'), np.datetime64('2020-02-22')))
+plt.xticks([np.datetime64('2020-01-22') + np.timedelta64(d, 'D') for d in range(0, 15)])
+ax.set_xticklabels(range(0, 15))
+plt.xlabel('Days since onset for each Country')
+
 plt.ylim((0, 30000))
-ax.set_xticklabels([])
 ax.set_yticklabels(['0' if x == 0 else '{:.0f}k'.format(int(x) / 1000) for x in ax.get_yticks().tolist()])
-plt.legend(loc='lower right')
+plt.ylabel('Confirmed infections')
+
+plt.legend(title='Countries', loc='lower right')
 
 if should_print:
   plt.savefig(dirname / f'figures/growth_rate.png', bbox_inches='tight', dpi=300, format='png')
 
 plt.show()
 
-#%% figure 2
-def sigmoid (x, A, h, slope, C):
-  return 1 / (1 + np.exp ((x - h) / slope)) *  A + C
+#%% plot growth curves
+def sigmoid (x, A, slope, offset):
+  return A / (1 + np.exp ((x - (offset + 17.75)) / slope))
 
-def fit_to_sigmoid (df, all_dates):
+def fit_to_sigmoid (df, offset, all_dates):
   dates = (df['date'] - start_date) / np.timedelta64(1, 'D')
   p, _ = curve_fit(
-    sigmoid,
+    lambda x, A, slope: sigmoid(x, A, slope, offset),
     dates,
     df['count'],
-    p0=[ 80000, 17, -5, -1000],
+    p0=[ 80000, -5],
     bounds=(
-      [-np.inf, -np.inf, -np.inf, -np.inf],
-      [np.inf, np.inf, -0.01, np.inf]
+      [-np.inf, -np.inf],
+      [np.inf, -0.01]
     ),
     maxfev=5000,
   )
-  return sigmoid((all_dates - start_date) / np.timedelta64(1, 'D'), *p), p
+  return sigmoid((all_dates - start_date) / np.timedelta64(1, 'D'), *p, offset), p
 
-# Plots the data
+#%% Plots the data
 fig = plt.figure(figsize=(12, 6))
 ax = fig.add_subplot(111)
 
-for country_name, offset, color_key in dat:
+for d in dat:
+  country_name, offset, color_key = itemgetter('name', 'offset', 'color')(d)
   country = countries[country_name]
-  fit, p = fit_to_sigmoid(country, all_dates)
-  print(country_name, *p)
+  fit, p = fit_to_sigmoid(country, offset, all_dates)
+  print(country_name, *p, offset)
 
   ax.plot(
     country['date'],
@@ -114,9 +145,20 @@ for country_name, offset, color_key in dat:
     linestyle=':'
   )
 
-plt.ylim((0, 100000))
+# plots the now line
+y_max = 200000
+now = np.datetime64('now').astype('datetime64[D]') - np.timedelta64(1, 'D')
+plt.vlines(now, ymin=0, ymax=y_max, colors=colors['very_light_gray'], linestyles='dashed')
+plt.annotate('Actual', xy=(now - np.timedelta64(1, 'D'), y_max - 5000), ha='right', va='top')
+plt.annotate('Estimated', xy=(now + np.timedelta64(1, 'D'), y_max - 5000), ha='left', va='top')
+
+plt.xlabel('Date')
+
+plt.ylim((0, y_max))
 ax.set_yticklabels(['0' if x == 0 else '{:.0f}k'.format(int(x) / 1000) for x in ax.get_yticks().tolist()])
-plt.legend(loc='lower right')
+plt.ylabel('Confirmed infections')
+
+plt.legend(title='Countries', loc='lower right')
 
 if should_print:
   plt.savefig(dirname / f'figures/growth_estimations.png', bbox_inches='tight', dpi=300, format='png')
